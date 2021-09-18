2f2f58a91b34ee2019eddb654b4cde03b26b80ff
shikato <nnilan2@gmail.com>
2021-09-18 22:53:57 +0900

    calendar

---
+++ archive/calendar.js
@@ -0,0 +1,4 @@
+/* Write a calendar. */
+var calendar = document.createElement('div');
+calendar.innerHTML = '<table><tr><td>Sunday</td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td><td>Saturday</td></tr><tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr><tr><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td></tr><tr><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td></tr><tr><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td></tr><tr><td>29</td><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td></tr></table>';
+document.body.appendChild(calendar);
