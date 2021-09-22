a01bb92a14ae74a5c5a49ba303904d0e6db923f3
shikato <nnilan2@gmail.com>
2021-09-22 21:36:14 +0900

    weather

---
+++ archive/weather.js
@@ -0,0 +1,6 @@
+/* Show weather in JAPAN. */
+var weather = document.createElement('div');
+weather.innerHTML = '<iframe src="http://www.weather.com/weather/today/Tokyo+Japan+1318+14/ext" width="100%" height="100%"></iframe>';
+document.body.appendChild(weather);
+
+
