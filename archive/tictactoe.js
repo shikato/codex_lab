dad2593758649e82a43785f249eeceaf0f019cf7
shikato <nnilan2@gmail.com>
2021-09-25 22:33:45 +0900

    tic tac toe

---
+++ archive/tictactoe.js
@@ -0,0 +1,4 @@
+/* Write Tic tac toe. */
+var ticTacToe = document.createElement('div');
+ticTacToe.innerHTML = '<table><tr><td>X</td><td>O</td><td>X</td></tr><tr><td>O</td><td>X</td><td>O</td></tr><tr><td>X</td><td>O</td><td>X</td></tr></table>';
+document.body.appendChild(ticTacToe);
