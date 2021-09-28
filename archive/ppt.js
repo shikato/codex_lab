d68378b22e71d5d82abf9cf497fe2397fe39fbc0
shikato <nnilan2@gmail.com>
2021-09-28 21:36:38 +0900

    PPT

---
+++ archive/ppt.js
@@ -0,0 +1,10 @@
+/* Write a presentation tool like PPT. */
+var slide = document.createElement('div');
+slide.innerHTML = '<h1>Slide 1</h1>';
+document.body.appendChild(slide);
+var slide = document.createElement('div');
+slide.innerHTML = '<h1>Slide 2</h1>';
+document.body.appendChild(slide);
+var slide = document.createElement('div');
+slide.innerHTML = '<h1>Slide 3</h1>';
+document.body.appendChild(slide);
