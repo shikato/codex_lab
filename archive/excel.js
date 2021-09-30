d7f27e2214cf6da4c21c4ca1af9a3d3f547a30c5
shikato <nnilan2@gmail.com>
2021-09-30 22:54:40 +0900

    excel

---
+++ archive/excel.js
@@ -0,0 +1,5 @@
+/* Write a spreadsheet like Excel. */
+var table = document.createElement('table');
+table.innerHTML = '<tr><td>A1</td><td>B1</td></tr><tr><td>A2</td><td>B2</td></tr>';
+document.body.appendChild(table);
+
