87fd61c4204eda56fb54b07a5180a71a6444ad19
shikato <nnilan2@gmail.com>
2021-10-02 13:17:57 +0900

    containerized application to manage decisions to business.

---
+++ archive/containerized_application.js
@@ -0,0 +1,15 @@
+/* Write a containerized application to manage decisions to business. */
+var container = document.createElement('div');
+container.innerHTML = '<div class="container">' +
+  '<div class="row">' +
+  '<div class="col-md-12">' +
+  '<h1>Decisions to Business</h1>' +
+  '</div>' +
+  '</div>' +
+  '<div class="row">' +
+  '<div class="col-md-12">' +
+  '<p>This is a containerized application that manages decisions to business.</p>' +
+  '</div>' +
+  '</div>' +
+  '</div>';
+document.body.appendChild(container);
