af39fa0858c9dcf76d764465c09e466283fc6c1c
shikato <nnilan2@gmail.com>
2021-10-06 22:53:53 +0900

    video player

---
+++ archive/video_player.js
@@ -0,0 +1,6 @@
+/* Write a video player. */
+var video = document.createElement('video');
+video.src = 'http://www.w3schools.com/tags/movie.mp4';
+video.controls = true;
+document.body.appendChild(video);
+
