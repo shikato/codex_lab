0b88c2ed3efb72a526798650b597df3b95885f6d
shikato <nnilan2@gmail.com>
2021-10-08 19:16:03 +0900

    CAD

---
+++ archive/cad.js
@@ -0,0 +1,8 @@
+/* Write a computer-aided design application. */
+var canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 500;
+document.body.appendChild(canvas);
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#FF0000';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
