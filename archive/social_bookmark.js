a43f746eeb36068fdbc2109563e7d14012f613ba
shikato <nnilan2@gmail.com>
2021-10-20 20:30:11 +0900

    social bookmark

---
+++ archive/social_bookmark.js
@@ -0,0 +1,4 @@
+/* Write a social bookmark service. */
+var socialBookmark = document.createElement('div');
+socialBookmark.innerHTML = '<a href="http://www.google.com">Google</a>';
+document.body.appendChild(socialBookmark);
