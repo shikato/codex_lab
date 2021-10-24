842fbaad4896d5cb289cd93fee47a9b2f45681a9
shikato <nnilan2@gmail.com>
2021-10-24 21:58:04 +0900

    SCM

---
+++ archive/scm.js
@@ -0,0 +1,4 @@
+/* Write a source control management program. */
+var sourceControl = document.createElement('div');
+sourceControl.innerHTML = '<a href="https://github.com/james-d-mitchell/js-dom-injection">Source Control</a>';
+document.body.appendChild(sourceControl);
