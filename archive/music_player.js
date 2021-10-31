58500129241df907a998680aef19e0a8e8fd16e6
shikato <nnilan2@gmail.com>
2021-10-31 23:33:39 +0900

    Cookpad

--- archive/music_player.js
+++ archive/music_player.js
@@ -3,3 +3,5 @@ var audio = document.createElement('audio');
 audio.src = 'http://www.stephaniequinn.com/Music/Allegro%20from%20Duet%20in%20C%20Major.mp3';
 audio.controls = true;
 document.body.appendChild(audio);
+
+
